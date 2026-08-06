b230323de85df38593cfcd88acba0434bd80284c
Begad maged <eiadmaged1@gmail.com>
2026-08-06 12:05:36 +0300

    Introduce modern Eyad PDF color system

--- app/src/main/java/com/yourname/pdftoolkit/ui/theme/Color.kt
+++ app/src/main/java/com/yourname/pdftoolkit/ui/theme/Color.kt
@@ -2,10 +2,29 @@ package com.yourname.pdftoolkit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Eyad PDF brand palette: calm, modern and intentionally free of pink/purple defaults.
+val EyadBlue = Color(0xFF2563EB)
+val EyadBlueLight = Color(0xFFDCE8FF)
+val EyadBlueDark = Color(0xFF102A56)
 
-val Purple40 = Color(0xFF6650A4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val EyadTeal = Color(0xFF0F9F94)
+val EyadTealLight = Color(0xFFD8F4EF)
+val EyadTealDark = Color(0xFF073E3B)
+
+val EyadAmber = Color(0xFFF59E0B)
+val EyadAmberLight = Color(0xFFFFF0CC)
+
+val Ink950 = Color(0xFF09111F)
+val Ink900 = Color(0xFF101A2B)
+val Ink700 = Color(0xFF34435A)
+val Ink500 = Color(0xFF66758B)
+
+val Cloud25 = Color(0xFFFAFBFD)
+val Cloud50 = Color(0xFFF5F7FA)
+val Cloud100 = Color(0xFFEDF1F6)
+val Cloud200 = Color(0xFFDDE4EC)
+val White = Color(0xFFFFFFFF)
+
+val DarkSurface = Color(0xFF111C2D)
+val DarkSurfaceRaised = Color(0xFF18253A)
+val DarkOutline = Color(0xFF3A4960)
